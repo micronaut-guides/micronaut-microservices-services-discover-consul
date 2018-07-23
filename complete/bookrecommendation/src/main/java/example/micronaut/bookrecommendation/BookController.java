60553029a35cdc00f8a222754107a0652f151a4e
sdelamo <sergio.delamo@softamo.com>
2018-07-23 16:56:41 +0200

    remove blockingGet()

--- complete/bookrecommendation/src/main/java/example/micronaut/bookrecommendation/BookController.java
+++ complete/bookrecommendation/src/main/java/example/micronaut/bookrecommendation/BookController.java
@@ -1,13 +1,9 @@
 package example.micronaut.bookrecommendation;
 
-import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.http.client.exceptions.HttpClientException;
 import io.reactivex.Flowable;
-import io.reactivex.Single;
 import io.reactivex.schedulers.Schedulers;
-import org.reactivestreams.Publisher;
 
 @Controller("/books") // <1>
 public class BookController {
@@ -24,17 +20,9 @@ public class BookController {
     @Get("/") // <3>
     public Flowable<BookRecommendation> index() {
         return bookCatalogueOperations.findAll()
-                .parallel(10)
-                .runOn(Schedulers.io())
-                .filter(b -> {
-                    try {
-                        Single<HttpResponse<Boolean>> httpResponseSingle = bookInventoryOperations.stock(b.getIsbn());
-                        return httpResponseSingle.blockingGet().body();
-                    } catch (HttpClientException e) {
-                        return Boolean.FALSE;
-                    }
-                })
-                .map(b -> new BookRecommendation(b.getName()))
-                .sequential();
+                .flatMapMaybe(b -> bookInventoryOperations.stock(b.getIsbn())
+                                    .filter(rsp -> rsp.body())
+                                    .map(rsp -> b))
+                .map(book -> new BookRecommendation(book.getName()));
     }
 }
