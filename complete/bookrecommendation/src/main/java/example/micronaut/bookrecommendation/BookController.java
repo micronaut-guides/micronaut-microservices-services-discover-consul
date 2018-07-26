728b53d4afb6642728470840cbb42e743e968248
sdelamo <sergio.delamo@softamo.com>
2018-07-26 17:30:56 +0200

    Capture not found book 404
    See: https://github.com/micronaut-projects/micronaut-core/issues/440

--- complete/bookrecommendation/src/main/java/example/micronaut/bookrecommendation/BookController.java
+++ complete/bookrecommendation/src/main/java/example/micronaut/bookrecommendation/BookController.java
@@ -1,9 +1,17 @@
 package example.micronaut.bookrecommendation;
 
 import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.client.exceptions.HttpClientException;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
 import io.reactivex.Flowable;
+import io.reactivex.Maybe;
+import io.reactivex.Single;
+import org.reactivestreams.Publisher;
+
+import java.util.Collections;
 
 @Controller("/books") // <1>
 public class BookController {
@@ -17,12 +25,22 @@ public class BookController {
         this.bookInventoryOperations = bookInventoryOperations;
     }
 
+
     @Get("/") // <3>
     public Flowable<BookRecommendation> index() {
         return bookCatalogueOperations.findAll()
                 .flatMapMaybe(b -> bookInventoryOperations.stock(b.getIsbn())
-                                    .filter(HttpResponse::body)
-                                    .map(rsp -> b))
-                .map(book -> new BookRecommendation(book.getName()));
+                        .onErrorResumeNext(throwable -> {
+                            if (throwable instanceof HttpClientResponseException) {
+                                HttpClientException ex = ((HttpClientResponseException) throwable);
+                                if (((HttpClientResponseException) ex).getResponse().getStatus().equals(HttpStatus.NOT_FOUND)) {
+                                    return Single.just(HttpResponse.ok(Boolean.FALSE));
+                                }
+                            }
+                            return Single.error(throwable);
+                        })
+                        .filter(HttpResponse::body)
+                        .map(rsp -> b)
+                ).map(book -> new BookRecommendation(book.getName()));
     }
 }
